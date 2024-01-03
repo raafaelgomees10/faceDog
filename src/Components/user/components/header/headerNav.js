1e1a6b2733aacf5c1a3481e46987a78e018a845b
Rafael Gomes <raafaelgomees10@gmail.com>
2024-01-03 16:14:42 -0300

    create menu mobile, create new hook useMedia, add styles for buttons on mobile

--- src/Components/user/components/header/headerNav.js
+++ src/Components/user/components/header/headerNav.js
@@ -1,14 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import * as S from "./styles.js";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../../UserContext";
 import { ReactComponent as FeedIcon } from "../../../../Assets/feed.svg";
 import { ReactComponent as SairIcon } from "../../../../Assets/sair.svg";
 import { ReactComponent as AdicionarIcon } from "../../../../Assets/adicionar.svg";
 import { ReactComponent as EstatisticasIcon } from "../../../../Assets/estatisticas.svg";
+import useMedia from "../../../../Hooks/useMedia.js";
 
 const HeaderNav = () => {
-  const [mobile, setMobile] = useState(null);
+  const [mobileMenu, setMobileMenu] = useState(false);
   const { userLogout } = useContext(UserContext);
   const navigate = useNavigate();
 
@@ -17,28 +18,49 @@ const HeaderNav = () => {
     navigate("/login");
   };
 
+  const mobile = useMedia("(max-width:767px)");
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <S.Nav>
-      <S.NavLinks to="/conta" end>
-        <FeedIcon />
-        {mobile && "Minhas Photos"}
-      </S.NavLinks>
+    <>
+      {mobile && (
+        <S.MobileButton
+          className={mobileMenu && "active"}
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></S.MobileButton>
+      )}
 
-      <S.NavLinks to="/conta/estatisticas">
-        <EstatisticasIcon />
-        {mobile && "Estatisticas"}
-      </S.NavLinks>
+      <S.Nav
+        className={`${mobile && "navMobile"} ${
+          mobileMenu && "navMobileActive"
+        }`}
+      >
+        <S.NavLinks to="/conta" end>
+          <FeedIcon />
+          {mobile && "Minhas Photos"}
+        </S.NavLinks>
 
-      <S.NavLinks to="/conta/postar">
-        <AdicionarIcon />
-        {mobile && "Adicionar Foto"}
-      </S.NavLinks>
+        <S.NavLinks to="/conta/estatisticas">
+          <EstatisticasIcon />
+          {mobile && "Estatisticas"}
+        </S.NavLinks>
 
-      <S.CustomButton onClick={handleLogout}>
-        <SairIcon />
-        {mobile && "Sair"}
-      </S.CustomButton>
-    </S.Nav>
+        <S.NavLinks to="/conta/postar">
+          <AdicionarIcon />
+          {mobile && "Adicionar Foto"}
+        </S.NavLinks>
+
+        <S.CustomButton onClick={handleLogout}>
+          <SairIcon />
+          {mobile && "Sair"}
+        </S.CustomButton>
+      </S.Nav>
+    </>
   );
 };
 
--- src/Components/user/components/header/styles.js
+++ src/Components/user/components/header/styles.js
@@ -13,6 +13,54 @@ export const Nav = styled.nav`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 1rem;
+
+  &.navMobile {
+    display: block;
+    position: absolute;
+    top: 70px;
+    right: 0px;
+    padding: 0.1rem;
+    background: #fff;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+    border-radius: 0.2rem;
+    transform: translateX(-10px);
+    opacity: 0;
+    pointer-events: none;
+
+    > a,
+    > button {
+      justify-content: flex-start;
+      display: flex;
+      align-items: center;
+      background: none;
+      width: 100%;
+      border: none;
+      border-bottom: 1px solid #eee;
+      padding: 0.5rem 0;
+      cursor: pointer;
+      > svg {
+        margin-right: 0.5rem;
+      }
+    }
+    > button {
+      border-bottom: none;
+    }
+
+    > a:hover,
+    > button:hover {
+      > svg > * {
+        fill: #fb1;
+      }
+    }
+  }
+
+  &.navMobileActive {
+    transition: 0.3s;
+    transform: initial;
+    opacity: 1;
+    z-index: 100;
+    pointer-events: initial;
+  }
 `;
 
 const baseStyles = css`
@@ -52,4 +100,50 @@ export const NavLinks = styled(NavLink)`
 
 export const CustomButton = styled.button`
   ${baseStyles}
+`;
+
+export const MobileButton = styled.button`
+  ${baseStyles}
+  padding: 0px;
+  &::after {
+    content: "";
+    display: block;
+    width: 1.2rem;
+    height: 2px;
+    background-color: currentColor;
+    border-radius: 2px;
+    box-shadow: 0 6px currentColor, 0 -6px currentColor;
+    transition: 0.2s;
+  }
+
+  &:focus,
+  &:hover,
+  &.active {
+    /* &${(props) => props.active} { */
+    outline: none;
+    background: #fff;
+    box-shadow: 0 0 0 3px #fea;
+    border-color: #fb1;
+    color: #fb1;
+  }
+
+  /* ${(props) =>
+    props.active &&
+    css`
+      &::after {
+        transform: rotate(90deg);
+        width: 4px;
+        height: 4px;
+        box-shadow: 0 8px currentColor, 0 -8px currentColor;
+      }
+    `}; */
+
+  &.active {
+    &::after {
+      transform: rotate(90deg);
+      width: 4px;
+      height: 4px;
+      box-shadow: 0 8px currentColor, 0 -8px currentColor;
+    }
+  }
 `;
